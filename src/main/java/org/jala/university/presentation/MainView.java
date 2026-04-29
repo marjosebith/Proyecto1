0068ae372275494a398d491a12b113c2628ba649
Carolina Vallejos <Carolina.Vallejos0725@jala.university>
2026-04-29 13:06:27 -0400

    HU26: implement service management module with JavaFX and show form

--- src/main/java/org/jala/university/presentation/MainView.java
+++ src/main/java/org/jala/university/presentation/MainView.java
@@ -1,19 +1,24 @@
 package org.jala.university.presentation;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import org.jala.university.commons.presentation.ViewSwitcher;
 public final class MainView extends Application {
-
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
     @Override
-    public void start(Stage primaryStage) {
-        Scene scene = new Scene(new Pane());
-        ViewSwitcher.setup(primaryStage, scene);
-        ViewSwitcher.switchTo(ExternalPaymentView.MAIN.getView());
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("External Payment Module Application");
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                MainView.class.getResource("/forms/ServicesView.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        stage.setTitle(" Ver Servicios");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
